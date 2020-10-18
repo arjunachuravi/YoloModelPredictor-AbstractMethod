dec2ff3296511143f4224ec4ba7588dbbb4ea1a7
arjunachuravi <arjunachuravi@gmail.com>
2020-10-18 14:58:12 +0530

    no message

--- train_test_prep.py
+++ train_test_prep.py
@@ -5,20 +5,14 @@ from os.path import isfile, join
 from pathlib import Path as path
 from sklearn.model_selection import train_test_split
 
-# training_img_data_path      = path("trainingData\images")
-# training_label_data_path    = path("trainingData\labels")
-# label_file                  = "labelfile.txt"
-
 def train_test_preparation_yolo(
     training_img_data_path,training_label_data_path,label_file ):
 
-    destination_path            = "Data"
-    train_file_gen_path         = path(f"{destination_path}\\training.txt")
-    test_file_gen_path          = path(f"{destination_path}\\testing.txt")
-    names_file                  = path(f"{destination_path}\\labels.names")
-    data_file                   = path(f"{destination_path}\\cov.data")
+    train_file_gen_path         = "training.txt"
+    test_file_gen_path          = "testing.txt"
+    names_file                  = "labels.names"
+    data_file                   = "cov.data"
 
-    os.mkdir(destination_path)
     image_files = [f for f in listdir(training_img_data_path) if isfile(join(training_img_data_path, f))]
     labels_file = [f for f in listdir(training_label_data_path) if isfile(join(training_label_data_path, f))]
 
